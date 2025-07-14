8a5e51ef5d7a769bdd8bdc7956dcb889f0f4ade6
ajitnk-lab <134615934+ajitnk-lab@users.noreply.github.com>
2025-07-14 17:49:44 +0530

    Create infrastructure package with __init__.py

---
+++ infrastructure/__init__.py
@@ -0,0 +1,8 @@
+"""
+Infrastructure package for Flask Todo CDK application.
+
+This package contains all CDK stack definitions and infrastructure components
+for the serverless Flask todo application.
+"""
+
+__version__ = "1.0.0"
